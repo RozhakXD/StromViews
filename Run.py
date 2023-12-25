9c379d61a8362938c8463d2e63e7817621ec3dfb
Rozhak <65714340+RozhakXD@users.noreply.github.com>
2023-12-25 11:04:31 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
             print(f"Perangkat {platform.machine()} bit, tidak dapat menjalankan aarch64 atau x86_64!")
             exit()
     except (KeyboardInterrupt):
-        subprocess.call(["./x86_64"])
+        exit()
     except (Exception) as e:
         print(f"[Error] {str(e).capitalize()}!")
         exit()
